1fa4698741e8b65c8ce8c4d99f9e049e214302e4
mostghoste <sarunas.jacikas+github@gmail.com>
2024-05-05 01:30:22 +0300

    Add prediction card design

---
+++ src/lib/components/PredictResults.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+</script>
+
+<div
+    class="flex justify-start items-start flex-col bg-base-200 rounded-md shadow-xl p-4 gap-2"
+>
+    <header class="flex justify-between w-full">
+        <h2 class="m-0">Atlik spėjimą</h2>
+        <p class="text-right m-0">Turnyras 1 <br />16/35</p>
+    </header>
+
+    <div
+        class="w-full min-w-64 h-fit p-4 bg-green-900 rounded-md shadow-gray-900 shadow-md"
+    >
+        <div class="flex h-24 gap-2 justify-between">
+            <!-- Left icon -->
+            <div
+                class="rounded-md h-full aspect-square bg-gray-800 shadow-gray-900 shadow-md"
+            ></div>
+            <!-- Middle -->
+            <div class="flex flex-col justify-end items-center gap-2">
+                <div
+                    class="mx-2 w-8 h-2 bg-slate-300 shadow-gray-800 shadow-md rounded-sm"
+                ></div>
+                <input
+                    type="text"
+                    class="bg-gray-800 input w-20 shadow-gray-900 shadow-md rounded-md text-center h-fit py-1 placeholder:text-gray-600 text-gray-200"
+                    placeholder="spėk"
+                />
+            </div>
+            <!-- Right icon -->
+            <div
+                class="rounded-md h-full aspect-square bg-gray-800 shadow-gray-900 shadow-md"
+            ></div>
+        </div>
+    </div>
+
+    <footer class="flex justify-end items-end w-full gap-2">
+        <button class="btn bg-slate-300 border-none shadow-gray-400 shadow-md"
+            >Atgal</button
+        >
+        <button class="btn btn-primary shadow-gray-400 shadow-md">Toliau</button
+        >
+    </footer>
+</div>
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
     import CreateTournament from "$lib/components/CreateTournament.svelte";
     import EditTournament from "$lib/components/EditTournament.svelte";
+    import PredictResults from "$lib/components/PredictResults.svelte";
 </script>
 
-<div class="prose flex flex-col gap-4">
-    <div class="bg-base-200 rounded-md">
-        <h1 class="m-4">Totalizatorius</h1>
+<div class="flex gap-4 m-4">
+    <div class="prose flex flex-col gap-4">
+        <div class="bg-base-200 rounded-md flex justify-center">
+            <h1 class="m-4">Totalizatorius</h1>
+        </div>
+        <CreateTournament></CreateTournament>
+        <EditTournament></EditTournament>
     </div>
-    <CreateTournament></CreateTournament>
-    <EditTournament></EditTournament>
+    <div class="prose flex flex-col gap-4">
+        <PredictResults></PredictResults>
+    </div>
 </div>
 
 <style>
