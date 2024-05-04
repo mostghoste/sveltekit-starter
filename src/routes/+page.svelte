fe02852de4de641ded7e62a2a593fb79da210c97
mostghoste <sarunas.jacikas+github@gmail.com>
2024-05-04 21:20:29 +0300

    Add tournament adding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import CreateTournament from "$lib/components/CreateTournament.svelte";
+</script>
+
+<h1>Totalizatorius</h1>
+<CreateTournament></CreateTournament>
+
+<style>
+</style>
