08fe0a0f72681bc5f26e99a9a6a26e35fca2ef74
mostghoste <sarunas.jacikas+github@gmail.com>
2024-05-04 21:08:44 +0300

    fix eyesore

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<main class="bg">
+    <slot />
+</main>
+
+<style>
+    .bg {
+        background: #111111;
+        width: 100%;
+        height: 100vh;
+        margin: 0;
+        color: #bbbbbb;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+</style>
