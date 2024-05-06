3abc91e4d5d42978657c3eb0ee20621000c0ae96
mostghoste <sarunas.jacikas+github@gmail.com>
2024-05-06 20:13:53 +0300

    Configure vite

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,24 @@
-import adapter from "@sveltejs/adapter-node";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: [vitePreprocess({})],
-
-  kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
-  },
+export default {
+    kit: {
+        adapter: adapter(),
+        vite: {
+            server: {
+                proxy: {
+                    '/api': {
+                        target: 'http://185.34.52.111:8090',
+                        changeOrigin: true,
+                        rewrite: path => path.replace(/^\/api/, '')
+                    }
+                },
+                cors: {
+                    origin: "https://mostghoste.lt",
+                    credentials: true
+                }
+            }
+        }
+    },
+    preprocess: preprocess()
 };
-
-export default config;
